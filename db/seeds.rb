8dac9d4a6e433b28158cc491b47edb0f26c233bd
zoebisch <zoebisch@gmail.com>
2017-12-07 10:22:20 -0500

    functional seed file and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-house = House.create([{ address: "101 Seattle Way, 98119"}])
-area = Area.new([{name: "living room"}])
-area2 = Area.new([{name: "bathroom"}])
-area3 = Area.new([{name: "hallway"}])
-project = Project.new([{name: "paint walls", status: "not started"}])
-project2 = Project.new([{name: "fix toilet"}])
-material = Material.create([{name: "white paint", quantity_on_hand: "2", unit: "gallon", value_per_unit: "25"}])
-material2 = Material.create([{name: "supply hose", quantity_on_hand: "1", unit: "hose", value_per_unit: "4"}])
-material3 = Material.create([{name: "teflon tape", quantity_on_hand: "1", unit: "roll", value_per_unit: "2"}])
-binding.pry
+house = House.new(address: "101 Seattle Way, 98119")
+area = Area.new(name: "living room")
+area2 = Area.new(name: "bathroom")
+area3 = Area.new(name: "hallway")
+project = Project.new(name: "paint walls", status: "not started")
+project2 = Project.new(name: "fix toilet", status: "in progress")
+material = Material.new(name: "white paint", quantity_on_hand: "2", unit: "gallon", value_per_unit: "25")
+material2 = Material.new(name: "supply hose", quantity_on_hand: "1", unit: "hose", value_per_unit: "4")
+material3 = Material.new(name: "teflon tape", quantity_on_hand: "1", unit: "roll", value_per_unit: "2")
 house.areas << area
 house.areas << area2
 house.areas << area3
+house.projects << project
+house.projects << project2
+house.save
+material.save
+material2.save
+material3.save
