1f1449efcec66673554681c4d0eb668f49512fe2
zoebisch <zoebisch@gmail.com>
2017-12-08 09:19:22 -0500

    added logic that if item is already associated with project then update quantity

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,16 +5,20 @@ class Project < ActiveRecord::Base
   has_many :materials, through: :items
 
   def add_material(material_id, quantity)
-    binding.pry
     material = Material.find(material_id)
     if material.quantity_on_hand < quantity
       #TODO: error if not enough materials on hand
     else
-      item = Item.new(material_id: material_id)
-      item.quantity = quantity
+      if self.items.find(material_id)
+        binding.pry
+        item = self.items.find(material_id)
+        item.quantity += quantity
+      else
+        item = Item.new(material_id: material_id)
+        item.quantity = quantity
+      end
       self.items << item
       self.save
-      binding.pry
     end
     item
   end
