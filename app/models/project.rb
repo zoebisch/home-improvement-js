6308ab6e7c326940b671b75abd222a0743598a9f
zoebisch <zoebisch@gmail.com>
2017-12-08 08:16:48 -0500

    added ability to add materials to project and hence items)

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,17 @@ class Project < ActiveRecord::Base
   has_many :areas, through: :house
   has_many :items
   has_many :materials, through: :items
+
+  def add_material(material_id, quantity)
+    material = Material.find_by(id: material_id)
+    binding.pry
+    if material.quantity_on_hand < quantity
+      #TODO: error if not enough materials on hand
+    else
+      binding.pry
+      self.materials << material
+      self.save
+    end
+    material
+  end
 end
