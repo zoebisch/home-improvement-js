77df7e85770d5a6504777eb23d9801edb5339fc0
zoebisch <zoebisch@gmail.com>
2017-12-11 13:18:45 -0500

    add omniauth callback and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'houses#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
   resources :items
   resources :materials
   resources :projects
