9ae7f16677f3fd3ebd93d92d131d1e54c4aafc67
zoebisch <zoebisch@gmail.com>
2017-12-12 14:36:53 -0500

    created new, create routes for house with strong params (house_params). Can create new house from root directory

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,4 +2,21 @@ class HousesController < ApplicationController
   def show
     @house = House.find(params[:id])
   end
+
+  def new
+    @house = House.new()
+  end
+
+  def create
+    @house = House.new(params[:id])
+    @house.update(house_params)
+    redirect_to house_path(@house)
+  end
+
+private
+
+def house_params
+  params.require(:house).permit(:address)
+end
+
 end
