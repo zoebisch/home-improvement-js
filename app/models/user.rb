8bfe762e7cf1142b4d6ef10656e6c2761c485547
zoebisch <zoebisch@gmail.com>
2017-12-12 13:57:16 -0500

    Obscure sign_in/sign_in with facebook when NOT current_user, obscure sign_out when current_user. Functional login/logout/omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+  devise :database_authenticatable, :registerable,:rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
