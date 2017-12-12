c1826d98ea1927645a1d8615676e867de1148e2d
zoebisch <zoebisch@gmail.com>
2017-12-12 14:51:33 -0500

    added delete functionality for address, but only when there are no associated projects

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -13,6 +13,13 @@ class HousesController < ApplicationController
     redirect_to house_path(@house)
   end
 
+  def destroy
+    @house = House.find(params[:id])
+    @house.delete
+    @house.save
+    redirect_to houses_path
+  end
+
 private
 
 def house_params
