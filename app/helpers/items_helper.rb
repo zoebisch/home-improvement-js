b0971af166891c5453f2ef5bed6f52164add093e
zoebisch <zoebisch@gmail.com>
2017-12-15 11:27:43 -0500

    added projects and materials show (list). project uses items_helper material_lookup to manage links to materials from items list.

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,14 @@
 module ItemsHelper
+
+  def material_lookup(item)
+    material = Material.find(item.material_id)
+    binding.pry
+    if item.quantity == nil
+      #TODO this shouldn't happen. An item should be vailidated with quantity.
+      item.quantity = 0
+      item.save
+    end
+    link_to "#{material.name}, quantity needed: #{item.quantity}", material_path(material.id)
+  end
+
 end
