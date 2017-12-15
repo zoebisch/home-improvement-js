cadb629d3a32357fc97e9dfa090db8952757ba9c
zoebisch <zoebisch@gmail.com>
2017-12-15 16:12:14 -0500

    created edit and update action for house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -13,6 +13,16 @@ class HousesController < ApplicationController
     redirect_to house_path(@house)
   end
 
+  def edit
+    @house = House.find(params[:id])
+  end
+
+  def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
+    redirect_to house_path(@house)
+  end
+
   def destroy
     @house = House.find(params[:id])
     @house.delete
