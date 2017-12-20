e3de83eed7a687db58210ae1cfd321ca474eadae
zoebisch <zoebisch@gmail.com>
2017-12-19 22:00:03 -0500

    remove resources :items by itself. it is contained nested in projects. updated spec file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  
   root 'houses#index'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
-  resources :items
+  resources :houses do
+    resources :projects, only: [:index, :new]
+  end
+
+  resources :areas
+
+  resources :projects do
+    resources :items, only: [:index]
+  end
 
   resources :materials do
     collection do
@@ -9,14 +17,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :projects do
-    resources :items, only: [:index]
-  end
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks"}
 
-  resources :areas
-
-  resources :houses do
-    resources :projects, only: [:index, :new]
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec.md
+++ spec.md
@@ -19,6 +19,6 @@ Specs:
 
 Confirm:
 - [ ] The application is pretty DRY
-- [ ] Limited logic in controllers
-- [ ] Views use helper methods if appropriate
-- [ ] Views use partials if appropriate
+- [x] Limited logic in controllers
+- [x] Views use helper methods if appropriate
+- [x] Views use partials if appropriate
