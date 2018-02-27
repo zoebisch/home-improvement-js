a9ca62271f0f701f28550a4cee4c59483c0838b1
Brian Zoebisch <zoebisch@gmail.com>
2018-02-26 22:29:43 -0500

    working through prototype. Final goal still foggy, but created Material object contructor and makeLink prototype method.  Not sure where I am going with this yet

--- app/assets/javascripts/homeimprovement.js
+++ app/assets/javascripts/homeimprovement.js
@@ -8,6 +8,18 @@ function parseMaterial(id) {
     debugger;
     $("#blah-" + id).html(data["name"]);
   });
+  let testMaterial = new Material('test_material', '2');
+  var blah = testMaterial.makeLink;
+  debugger;
   const stuff = JSON.parse(this.responseText)
   document.getElementById("stuff".innerHTML = stuff)
+}
+
+function Material(name, quantity_on_hand) {
+  this.name = name;
+  this.quantity_on_hand = quantity_on_hand;
+}
+
+Material.prototype.makeLink = function(){
+  return this.name + " " + this.quantity_on_hand
 }
