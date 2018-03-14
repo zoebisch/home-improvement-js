cdbbd7fa4455e5d56f4f7e4fd487a4ebf7750bd7
Brian Zoebisch <zoebisch@gmail.com>
2018-03-14 19:48:18 -0400

    moved filter button functionality into asset

--- app/assets/javascripts/homeimprovement.js
+++ app/assets/javascripts/homeimprovement.js
@@ -8,15 +8,29 @@ function parseMaterial(json) {
 }
 
 $(function () {
+  
   Handlebars.registerHelper('alert', function() {
     alert("material already exists")
   });
+
+  $('#filter').on("click", function(){
+    $.getJSON("/materials", function(data) {
+      var materialList = "";
+      var filtered = data.filter((material) => material.quantity_on_hand >= 100);
+      filtered.forEach(function(material) {
+        materialList += '<li><a href=' + "/materials/" + material["id"] + '>' + material["name"] + " - Quantity On Hand " + material["quantity_on_hand"] + '</a></li>'
+      });
+      $("#materials").html("")
+      $("#materials").append(materialList);
+    })
+  });
+
 })
 
 function Material(attributes) {
   this.id = attributes.id
   this.name = attributes.name;
-  this.quantity_on_hand = attributes.quantity_on_hand;
+  this.quantityOnHand = attributes.quantity_on_hand;
 }
 
 Material.prototype.renderLI = function(){
